97214a8bf01d40c75eea71aee99807f925ea7abd
Moussa Sidibe <moussa.sidibe@yoti.com>
2018-02-13 16:56:50 +0000

    SDK-248 : Export Country, AmlAddress and , AmlProfile module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const client = require('./src/client');
+const client = require('./src/client').YotiClient;
 const Country = require('./src/yoti_entity/country').Country;
 const AmlAddress = require('./src/yoti_entity/aml.address').AmlAddress;
 const AmlProfile = require('./src/yoti_entity/aml.profile').AmlProfile;
