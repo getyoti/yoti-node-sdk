6d2d3a3f843659014f58255f74d55407156f1c26
David Grayston <davidgrayston@googlemail.com>
2019-02-13 16:59:00 +0000

    SDK-584: Image object

--- src/yoti_common/attribute.converter.js
+++ src/yoti_common/attribute.converter.js
@@ -2,6 +2,8 @@
 
 const constants = require('./constants');
 const { DocumentDetails } = require('../data_type/document.details');
+const ImagePng = require('../data_type/image.png');
+const ImageJpeg = require('../data_type/image.jpeg');
 
 module.exports.AttributeConverter = class AttributeConverter {
   static convertValueBasedOnAttributeName(value, attrName) {
@@ -12,6 +14,32 @@ module.exports.AttributeConverter = class AttributeConverter {
     switch (attrName) {
       case constants.ATTR_DOCUMENT_DETAILS:
         return new DocumentDetails(value);
+      default:
+        return value;
+    }
+  }
+
+  static convertValueBasedOnContentType(value, contentType) {
+    if (!value) {
+      return null;
+    }
+
+    switch (contentType) {
+      // UNDEFINED should not be seen, and is used as an error placeholder
+      case constants.CONTENT_TYPE_UNDEFINED:
+        throw new Error('Wrong content type');
+      case constants.CONTENT_TYPE_STRING: // STRING means the value is UTF-8 encoded text.
+      case constants.CONTENT_TYPE_DATE: // Date as string in RFC3339 format (YYYY-MM-DD).
+        return value.toUTF8();
+      case constants.CONTENT_TYPE_BYTES: {
+        // Convert ByteArray to JSON
+        const attrValue = Buffer.from(value.toArrayBuffer()).toString();
+        return JSON.parse(attrValue);
+      }
+      case constants.CONTENT_TYPE_JPEG:
+        return new ImageJpeg(value);
+      case constants.CONTENT_TYPE_PNG:
+        return new ImagePng(value);
       default:
         return value;
     }
