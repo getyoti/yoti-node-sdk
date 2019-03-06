14b5320b175140b47f4df808e41dc38f016969ff
David Grayston <davidgrayston@googlemail.com>
2019-03-06 18:36:04 +0000

    SDK-759: Add DocumentImages object

--- tests/yoti-common.attribute.converter.spec.js
+++ tests/yoti-common.attribute.converter.spec.js
@@ -3,29 +3,51 @@ const fs = require('fs');
 const { AttributeConverter } = require('../src/yoti_common/attribute.converter');
 const protoRoot = require('../src/proto-root');
 const ImageJpeg = require('../src/data_type/image.jpeg');
+const MultiValue = require('../src/data_type/multi.value');
+const DocumentImages = require('../src/data_type/document.images');
+const constants = require('../src/yoti_common/constants');
 
 const multiValueAttribute = fs.readFileSync('./tests/sample-data/fixtures/attributes/multi-value.txt', 'utf8');
 
 const CONTENT_TYPE_MULTI_VALUE = 6;
 
+const getMultiValue = () => {
+  const protoInst = protoRoot.initializeProtoBufObjects();
+
+  const attribute = protoInst
+    .builder
+    .attrpubapi_v1
+    .Attribute
+    .decode(multiValueAttribute);
+
+  return AttributeConverter.convertValueBasedOnContentType(
+    attribute.value,
+    CONTENT_TYPE_MULTI_VALUE
+  );
+};
+
 describe('attributeConverter', () => {
   describe('#convertValueBasedOnContentType', () => {
-    it('should return multi value array of attributes', () => {
-      const protoInst = protoRoot.initializeProtoBufObjects();
+    it('should return multi value object', () => {
+      const multiValue = getMultiValue();
 
-      const attribute = protoInst
-        .builder
-        .attrpubapi_v1
-        .Attribute
-        .decode(multiValueAttribute);
+      expect(multiValue.values.length).to.equal(2);
+      expect(multiValue).to.be.instanceOf(MultiValue);
+      multiValue.getValues().forEach((item) => {
+        expect(item).to.be.instanceOf(ImageJpeg);
+      });
+    });
+  });
+  describe('#convertValueBasedOnAttributeName', () => {
+    it('should return document images object', () => {
+      const documentImages = AttributeConverter
+        .convertValueBasedOnAttributeName(getMultiValue(), constants.ATTR_DOCUMENT_IMAGES);
 
-      const value = AttributeConverter.convertValueBasedOnContentType(
-        attribute.value,
-        CONTENT_TYPE_MULTI_VALUE
-      );
-
-      expect(value[0]).to.be.instanceOf(ImageJpeg);
-      expect(value[1]).to.be.instanceOf(ImageJpeg);
+      expect(documentImages.values.length).to.equal(2);
+      expect(documentImages).to.be.instanceOf(DocumentImages);
+      documentImages.getImages().forEach((item) => {
+        expect(item).to.be.instanceOf(ImageJpeg);
+      });
     });
   });
 });
