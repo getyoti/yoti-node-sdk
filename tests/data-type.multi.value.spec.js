bc2e7325685d374eff672a3da65f3780e39c9a8b
David Grayston <davidgrayston@googlemail.com>
2019-03-07 15:30:45 +0000

    SDK-759: Apply filters to nested multi value items

--- tests/data-type.multi.value.spec.js
+++ tests/data-type.multi.value.spec.js
@@ -4,24 +4,85 @@ const Image = require('../src/data_type/image');
 const ImageJpeg = require('../src/data_type/image.jpeg');
 const ImagePng = require('../src/data_type/image.png');
 
-const testValues = [
+const getTestValues = () => [
   new ImageJpeg('image_1'),
   new ImagePng('image_2'),
   'test string',
-  {
-    name: 'test object',
-  },
-  [
-    'test',
-    'array',
-  ],
+  { name: 'test object' },
+  ['test', 'array'],
+  new MultiValue([
+    new ImageJpeg('image_3'),
+    new ImagePng('image_4'),
+    'test string 2',
+    { name: 'test object 2' },
+    ['test', 'array 2'],
+    new MultiValue([
+      new ImageJpeg('image_5'),
+      new ImagePng('image_6'),
+      'test string 3',
+      { name: 'test object 3' },
+      ['test', 'array 3'],
+    ]),
+  ]),
 ];
 
 describe('MultiValue', () => {
+  context('Should allow nesting', () => {
+    const multiValue = new MultiValue(getTestValues());
+    const thirdLevelMultiValue = multiValue.getValues()[5].getValues()[5];
+
+    expect(thirdLevelMultiValue).to.be.instanceOf(MultiValue);
+    expect(thirdLevelMultiValue.getValues().length).to.equal(5);
+    expect(thirdLevelMultiValue.getValues()[0]).to.be.instanceOf(ImageJpeg);
+    expect(thirdLevelMultiValue.getValues()[1]).to.be.instanceOf(ImagePng);
+  });
+  context('#applyFilters', () => {
+    it('should apply filters to all multi values', () => {
+      const multiValue = new MultiValue(getTestValues());
+      expect(multiValue.getValues().length).to.equal(6);
+
+      // Apply filters.
+      multiValue
+        .filterInstance(ImageJpeg)
+        .filterInstance(MultiValue);
+
+      // First level.
+      expect(multiValue.getValues().length).to.equal(2);
+
+      // Second level.
+      const secondLevelMultiValue = multiValue.getValues()[1];
+      expect(secondLevelMultiValue.getValues().length).to.equal(2);
+
+      // Third level.
+      const thirdLevelMultiValue = secondLevelMultiValue.getValues()[1];
+      expect(thirdLevelMultiValue.getValues().length).to.equal(1);
+    });
+    it('should allow combination of instance and type filters', () => {
+      const multiValue = new MultiValue(getTestValues());
+      expect(multiValue.getValues()).to.eql(getTestValues());
+
+      // Allow ImageJpeg instances, Array and MultiValue.
+      multiValue
+        .filterInstance(ImageJpeg)
+        .filterType('Array')
+        .filterType('MultiValue');
+      expect(multiValue.getValues().length).to.equal(3);
+      expect(multiValue.getValues()[0]).to.be.instanceOf(ImageJpeg);
+      expect(multiValue.getValues()[1]).to.eql(['test', 'array']);
+      expect(multiValue.getValues()[2]).to.be.instanceOf(MultiValue);
+
+      // Check filters applied to second level.
+      const secondLevelMultiValue = multiValue.getValues()[2];
+      expect(secondLevelMultiValue.getValues().length).to.equal(3);
+      expect(secondLevelMultiValue.getValues()[0]).to.be.instanceOf(ImageJpeg);
+      expect(secondLevelMultiValue.getValues()[1]).to.eql(['test', 'array 2']);
+      expect(secondLevelMultiValue.getValues()[2]).to.be.instanceOf(MultiValue);
+    });
+  });
   context('#filterInstance()', () => {
     it('should filter values by instance', () => {
-      const multiValue = new MultiValue(testValues);
-      expect(multiValue.getValues()).to.eql(testValues);
+      const multiValue = new MultiValue(getTestValues());
+      expect(multiValue.getValues()).to.eql(getTestValues());
 
       // Allow only ImageJpeg.
       multiValue.filterInstance(ImageJpeg);
@@ -36,7 +97,7 @@ describe('MultiValue', () => {
 
       // Reset filters.
       multiValue.resetFilters();
-      expect(multiValue.getValues()).to.eql(testValues);
+      expect(multiValue.getValues()).to.eql(getTestValues());
 
       // Allow any Image.
       multiValue.filterInstance(Image);
@@ -47,8 +108,8 @@ describe('MultiValue', () => {
   });
   context('#filterType()', () => {
     it('should filter values by type', () => {
-      const multiValue = new MultiValue(testValues);
-      expect(multiValue.getValues()).to.eql(testValues);
+      const multiValue = new MultiValue(getTestValues());
+      expect(multiValue.getValues()).to.eql(getTestValues());
 
       // Allow String and Object.
       multiValue
@@ -64,7 +125,7 @@ describe('MultiValue', () => {
 
       // Reset filters.
       multiValue.resetFilters();
-      expect(multiValue.getValues()).to.eql(testValues);
+      expect(multiValue.getValues()).to.eql(getTestValues());
 
       // Allow only ImageJpeg.
       multiValue.filterType('ImageJpeg');
