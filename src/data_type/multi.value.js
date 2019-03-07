46a763941ce9620ab3473b97e4ca31e5d8f74b1b
David Grayston <davidgrayston@googlemail.com>
2019-03-07 13:58:59 +0000

    SDK-759: Allow MultiValue to be filtered by instance or class name

--- src/data_type/multi.value.js
+++ src/data_type/multi.value.js
@@ -9,32 +9,62 @@ module.exports = class MultiValue {
   constructor(values) {
     this.originalValues = values;
     this.values = values;
-    this.filters = [];
+    this.filterInstances = [];
+    this.filterTypes = [];
   }
 
   /**
-   * Filter values by type.
+   * Filter values by their type.
    *
-   * @param {*} filterType
+   * @param {*} type
    */
-  filter(filterType) {
-    this.filters.push(filterType);
-
-    const filteredValues = [];
-    this.originalValues.forEach((value) => {
-      if (this.filters.find(t => value instanceof t)) {
-        filteredValues.push(value);
-      }
-    });
-    this.values = filteredValues;
-
+  filterInstance(type) {
+    this.filterInstances.push(type);
     return this;
+  }
+
+  /**
+   * Filter values by their constructor name.
+   *
+   * @param {*} type
+   */
+  filterType(type) {
+    this.filterTypes.push(type);
+    return this;
+  }
+
+  /**
+   * Apply all filters.
+   */
+  applyFilters() {
+    this.values = this.values.filter((value) => {
+      let allowedInstance = true;
+      let allowedType = true;
+      if (this.filterInstances.length > 0) {
+        allowedInstance = this.filterInstances.find(type => value instanceof type);
+      }
+      if (this.filterTypes.length > 0) {
+        allowedType = this.filterTypes.find(type => value.constructor.name === type);
+      }
+      return allowedType && allowedInstance;
+    });
+  }
+
+  /**
+   * Reset to original values.
+   */
+  resetFilters() {
+    this.values = this.originalValues;
+    this.filterInstances = [];
+    this.filterTypes = [];
   }
 
   /**
    * @returns {Array} List of filtered values.
    */
   getValues() {
+    this.values = this.originalValues;
+    this.applyFilters();
     return this.values;
   }
 };
