f4888bcb60555a65fe27a5ee31fbded33cc89026
David Grayston <davidgrayston@googlemail.com>
2019-03-07 13:58:59 +0000

    SDK-759: Allow MultiValue to be filtered by instance or class name

---
+++ tests/data-type.multi.value.spec.js
@@ -0,0 +1,74 @@
+const expect = require('chai').expect;
+const MultiValue = require('../src/data_type/multi.value');
+const ImageJpeg = require('../src/data_type/image.jpeg');
+const ImagePng = require('../src/data_type/image.png');
+
+const testValues = [
+  new ImageJpeg('image_1'),
+  new ImagePng('image_2'),
+  'test string',
+  {
+    name: 'test object',
+  },
+  [
+    'test',
+    'array',
+  ],
+];
+
+describe('MultiValue', () => {
+  context('#filterInstance()', () => {
+    it('should filter values by instance', () => {
+      const multiValue = new MultiValue(testValues);
+      expect(multiValue.getValues()).to.eql(testValues);
+
+      // Allow only ImageJpeg.
+      multiValue.filterInstance(ImageJpeg);
+      expect(multiValue.getValues().length).to.equal(1);
+      expect(multiValue.getValues()[0]).to.be.instanceOf(ImageJpeg);
+
+      // Also allow ImagePng.
+      multiValue.filterInstance(ImagePng);
+      expect(multiValue.getValues().length).to.equal(2);
+      expect(multiValue.getValues()[0]).to.be.instanceOf(ImageJpeg);
+      expect(multiValue.getValues()[1]).to.be.instanceOf(ImagePng);
+
+      // Reset filters.
+      multiValue.resetFilters();
+      expect(multiValue.getValues()).to.eql(testValues);
+    });
+  });
+  context('#filterType()', () => {
+    it('should filter values by type', () => {
+      const multiValue = new MultiValue(testValues);
+      expect(multiValue.getValues()).to.eql(testValues);
+
+      // Allow String and Object.
+      multiValue
+        .filterType('String')
+        .filterType('Object');
+      expect(multiValue.getValues().length).to.equal(2);
+      expect(multiValue.getValues()[0]).to.equal('test string');
+      expect(multiValue.getValues()[1]).to.eql({ name: 'test object' });
+
+      // Also allow Arrays.
+      multiValue.filterType('Array');
+      expect(multiValue.getValues().length).to.equal(3);
+
+      // Reset filters.
+      multiValue.resetFilters();
+      expect(multiValue.getValues()).to.eql(testValues);
+
+      // Allow only ImageJpeg.
+      multiValue.filterType('ImageJpeg');
+      expect(multiValue.getValues().length).to.equal(1);
+      expect(multiValue.getValues()[0]).to.be.instanceOf(ImageJpeg);
+
+      // Also allow Strings.
+      multiValue.filterType('String');
+      expect(multiValue.getValues().length).to.equal(2);
+      expect(multiValue.getValues()[0]).to.be.instanceOf(ImageJpeg);
+      expect(multiValue.getValues()[1]).to.equal('test string');
+    });
+  });
+});
--- tests/yoti-common.attribute.converter.spec.js
+++ tests/yoti-common.attribute.converter.spec.js
@@ -4,7 +4,6 @@ const { AttributeConverter } = require('../src/yoti_common/attribute.converter')
 const protoRoot = require('../src/proto-root');
 const ImageJpeg = require('../src/data_type/image.jpeg');
 const MultiValue = require('../src/data_type/multi.value');
-const DocumentImages = require('../src/data_type/document.images');
 const constants = require('../src/yoti_common/constants');
 
 const multiValueAttribute = fs.readFileSync('./tests/sample-data/fixtures/attributes/multi-value.txt', 'utf8');
@@ -30,23 +29,23 @@ describe('attributeConverter', () => {
   describe('#convertValueBasedOnContentType', () => {
     it('should return multi value object', () => {
       const multiValue = getMultiValue();
-
-      expect(multiValue.values.length).to.equal(2);
+      expect(multiValue.getValues().length).to.equal(2);
       expect(multiValue).to.be.instanceOf(MultiValue);
       multiValue.getValues().forEach((item) => {
         expect(item).to.be.instanceOf(ImageJpeg);
+        expect(item.getMimeType()).to.equal('image/jpeg');
       });
     });
   });
   describe('#convertValueBasedOnAttributeName', () => {
-    it('should return document images object', () => {
+    it('should return array of image objects', () => {
       const documentImages = AttributeConverter
         .convertValueBasedOnAttributeName(getMultiValue(), constants.ATTR_DOCUMENT_IMAGES);
 
-      expect(documentImages.values.length).to.equal(2);
-      expect(documentImages).to.be.instanceOf(DocumentImages);
-      documentImages.getImages().forEach((item) => {
+      expect(documentImages.length).to.equal(2);
+      documentImages.forEach((item) => {
         expect(item).to.be.instanceOf(ImageJpeg);
+        expect(item.getMimeType()).to.equal('image/jpeg');
       });
     });
   });
