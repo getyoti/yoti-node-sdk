e2aea1d9afd1d73886d32bd97eb0dfb614c26869
David Grayston <davidgrayston@googlemail.com>
2019-06-10 18:05:20 +0100

    SDK-660: Test coverage for YotiSignedTimeStamp

---
+++ tests/data_type/signed.timestamp.spec.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect;
+
+const { YotiSignedTimeStamp } = require('../../src/data_type/signed.timestamp');
+
+describe('YotiSignedTimeStamp', () => {
+  const signedTimestamp = new YotiSignedTimeStamp({
+    version: 1,
+    timestamp: new Date('2003-11-04T12:51:07Z'),
+  });
+
+  context('#getVersion()', () => {
+    it('it should return 1', () => {
+      expect(signedTimestamp.getVersion()).to.equal(1);
+    });
+  });
+  context('#getTimestamp()', () => {
+    it('it should return Date object', () => {
+      expect(signedTimestamp.getTimestamp()).to.be.a('Date');
+      expect(signedTimestamp.getTimestamp().toUTCString()).to.equal('Tue, 04 Nov 2003 12:51:07 GMT');
+    });
+  });
+});
