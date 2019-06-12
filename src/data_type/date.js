a69cd534de44594ab2995dc6be18449ad296d11e
David Grayston <davidgrayston@googlemail.com>
2019-06-12 14:29:00 +0100

    SDK-1000: Provide methods to get time and timestamp with microseconds

--- src/data_type/date.js
+++ src/data_type/date.js
@@ -1,4 +1,14 @@
 /**
+ * Formats date part with zero padding.
+ *
+ * @param {*} part
+ * @param {number} length
+ */
+function formatDatePart(part, length) {
+  return part.toString().padStart(length, '0');
+}
+
+/**
  * Date object with microsecond accuracy.
  *
  * @class YotiDate
@@ -19,6 +29,34 @@ class YotiDate extends Date {
    */
   getMicroseconds() {
     return this.microseconds;
+  }
+
+  /**
+   * Time with microseconds.
+   *
+   * @returns {string}
+   *   Time in format `{HH}:{MM}:{SS}.{mmmmmm}`
+   */
+  getMicrosecondTime() {
+    const hours = formatDatePart(this.getUTCHours(), 2);
+    const minutes = formatDatePart(this.getUTCMinutes());
+    const secondsWithMicroseconds = (this.getUTCSeconds() + (this.getMicroseconds() / 1000000))
+      .toFixed(6)
+      .padStart(9, '0');
+    return `${hours}:${minutes}:${secondsWithMicroseconds}`;
+  }
+
+  /**
+   * Returns ISO 8601 UTC timestamp with microseconds.
+   *
+   * @returns {string}
+   *   Timestamp in format `{YYYY}-{DD}-{MM}T{HH}:{MM}:{SS}.{mmmmmm}Z`
+   */
+  getMicrosecondTimestamp() {
+    const year = formatDatePart(this.getUTCFullYear(), 4);
+    const month = formatDatePart(this.getUTCMonth(), 2);
+    const day = formatDatePart(this.getUTCDate(), 2);
+    return `${year}-${month}-${day}T${this.getMicrosecondTime()}Z`;
   }
 }
 
