2e1989ff3ff304bbbf0f1c35689a7c55b5287327
Alex Burt <alex.burt@yoti.com>
2019-11-01 15:42:04 +0000

    SDK-1262: Change third party attribute extension builder to accept strings for definitions, and then build the definition with the value supplied

--- src/dynamic_sharing_service/extension/third.party.attribute.extension.builder.js
+++ src/dynamic_sharing_service/extension/third.party.attribute.extension.builder.js
@@ -2,7 +2,7 @@
 
 const Extension = require('./extension');
 const ThirdPartyAttributeExtensionContent = require('./third.party.attribute.extension.content');
-const AttributeDefinition = require('../attribute.definition');
+const AttributeDefinition = require('../../data_type/attribute.definition');
 const Validation = require('../../yoti_common/validation');
 
 const THIRD_PARTY_ATTRIBUTE_EXTENSION_TYPE = 'THIRD_PARTY_ATTRIBUTE';
@@ -19,14 +19,14 @@ class ThirdPartyAttributeExtensionBuilder {
   }
 
   withDefinition(definition) {
-    Validation.instanceOf(definition, AttributeDefinition, 'definition');
-    this.definitions.push(definition);
+    Validation.isString(definition, 'definition');
+    this.definitions.push(new AttributeDefinition(definition));
     return this;
   }
 
   withDefinitions(definitions) {
-    Validation.isArrayOfType(definitions, AttributeDefinition, 'definitions');
-    this.definitions = definitions;
+    Validation.hasOnlyStringValues(definitions, 'definitions');
+    this.definitions = definitions.map(def => new AttributeDefinition(def));
     return this;
   }
 
