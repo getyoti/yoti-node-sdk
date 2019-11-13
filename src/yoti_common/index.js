8f311b4638177093a71d9218f434fddfaa95e388
David Grayston <davidgrayston@googlemail.com>
2019-11-13 11:37:26 +0000

    SDK-1231: Decrypt extra data

--- src/yoti_common/index.js
+++ src/yoti_common/index.js
@@ -12,30 +12,58 @@ const ExtraDataConverter = require('./extra.data.converter');
 // Request methods that can include payload data.
 const methodsThatIncludePayload = ['POST', 'PUT', 'PATCH'];
 
-function decipherProfile(cipherText, key, iv) {
-  const decipher = forge.cipher.createDecipher('AES-CBC', key);
-  const data = forge.util.createBuffer();
+/**
+ * Decrypt wrapped key using provided pem key.
+ *
+ * @param {string} wrappedKey
+ * @param {string} pem
+ *
+ * @returns {string}
+ */
+function unwrapKey(wrappedKey, pem) {
+  const wrappedKeyBuffer = Buffer.from(wrappedKey, 'base64');
+  const privateKey = new NodeRSA(pem, 'pkcs1', { encryptionScheme: 'pkcs1' });
+  const unwrappedKey = privateKey.decrypt(wrappedKeyBuffer, 'base64');
+  return forge.util.decode64(unwrappedKey);
+}
 
-  data.putBytes(cipherText);
+/**
+ * Decrypt encrypted data using provided wrapped key.
+ *
+ * @param {string} encryptedData
+ * @param {string} wrappedReceiptKey
+ * @param {string} pem
+ *
+ * @returns {Buffer}
+ */
+function decryptEncryptedData(encryptedData, wrappedReceiptKey, pem) {
+  const decodedData = protoRoot.decodeEncryptedData(Buffer.from(encryptedData, 'base64'));
+
+  const iv = forge.util.decode64(decodedData.iv);
+  const cipherText = forge.util.decode64(decodedData.cipherText);
+
+  const data = forge.util
+    .createBuffer()
+    .putBytes(cipherText);
+
+  const decipher = forge.cipher
+    .createDecipher(
+      'AES-CBC',
+      unwrapKey(wrappedReceiptKey, pem)
+    );
 
   decipher.start({ iv });
   decipher.update(data);
   decipher.finish();
 
-  const cipherTextAsBytes = decipher.output.getBytes();
-
-  const attributeList = protoRoot.decodeAttributeList(Buffer.from(forge.util.encode64(cipherTextAsBytes), 'base64'));
-  return attributeList;
+  return Buffer.from(decipher.output.getBytes(), 'binary');
 }
 
-function unwrapKey(wrappedKey, pem) {
-  const wrappedKeyBuffer = Buffer.from(wrappedKey, 'base64');
-  const privateKey = new NodeRSA(pem, 'pkcs1', { encryptionScheme: 'pkcs1' });
-  const unwrappedKey = privateKey.decrypt(wrappedKeyBuffer, 'base64');
-
-  return unwrappedKey;
-}
-
+/**
+ * @param {string} httpMethod
+ *
+ * @returns {boolean}
+ */
 module.exports.requestCanSendPayload = (httpMethod) => {
   // Check if the request method can send payload data
   if (methodsThatIncludePayload.indexOf(httpMethod) === -1) {
@@ -45,12 +73,23 @@ module.exports.requestCanSendPayload = (httpMethod) => {
   return true;
 };
 
-module.exports.getRSASignatureForMessage = (message, pem) => {
-  const sign = crypto.createSign('RSA-SHA256');
-  sign.update(message);
-  return sign.sign(pem).toString('base64');
-};
+/**
+ * @param {string} message
+ * @param {string} pem
+ *
+ * @returns {string}
+ */
+module.exports.getRSASignatureForMessage = (message, pem) => crypto
+  .createSign('RSA-SHA256')
+  .update(message)
+  .sign(pem)
+  .toString('base64');
 
+/**
+ * @param {string} pem
+ *
+ * @returns {string}
+ */
 module.exports.getAuthKeyFromPem = (pem) => {
   const privateKey = forge.pki.privateKeyFromPem(pem);
   const publicKey = forge.pki.setRsaPublicKey(privateKey.n, privateKey.e);
@@ -60,38 +99,66 @@ module.exports.getAuthKeyFromPem = (pem) => {
   return p12b64;
 };
 
+/**
+ * @param {string} profileContent
+ * @param {string} wrappedReceiptKey
+ * @param {string} pem
+ *
+ * @returns {Object[]}
+ */
 module.exports.decryptProfileContent = (profileContent, wrappedReceiptKey, pem) => {
   const receiptNotEmpty = profileContent && Object.keys(profileContent).length > 0;
 
   if (receiptNotEmpty) {
-    const unwrappedKey = unwrapKey(wrappedReceiptKey, pem);
-    const decodedData = protoRoot.decodeEncryptedData(Buffer.from(profileContent, 'base64'));
-    const iv = forge.util.decode64(decodedData.iv);
-    const cipherText = forge.util.decode64(decodedData.cipherText);
-
-    return decipherProfile(cipherText, forge.util.decode64(unwrappedKey), iv);
+    const decryptedProfileData = decryptEncryptedData(profileContent, wrappedReceiptKey, pem);
+    return protoRoot.decodeAttributeList(decryptedProfileData);
   }
   console.log('Receipt data is empty');
   return [];
 };
 
+/**
+ * @param {Object} receipt
+ * @param {string} pem
+ *
+ * @returns {Object[]}
+ */
 module.exports.decryptCurrentUserReceipt = (receipt, pem) => this.decryptProfileContent(
   receipt.other_party_profile_content,
   receipt.wrapped_receipt_key,
   pem
 );
 
+/**
+ * @param {Object} receipt
+ * @param {string} pem
+ *
+ * @returns {Object[]}
+ */
 module.exports.decryptApplicationProfile = (receipt, pem) => this.decryptProfileContent(
   receipt.profile_content,
   receipt.wrapped_receipt_key,
   pem
 );
 
-module.exports.parseExtraData = (extraDataContent) => {
-  const extraDataNotEmpty = extraDataContent && Object.keys(extraDataContent).length > 0;
+/**
+ * @param {Object} receipt
+ * @param {string} pem
+ *
+ * @returns {ExtraData}
+ */
+module.exports.parseExtraData = (receipt, pem) => {
+  const extraDataNotEmpty = receipt.extra_data_content
+    && Object.keys(receipt.extra_data_content).length > 0;
 
   if (extraDataNotEmpty) {
-    return ExtraDataConverter.convertExtraData(extraDataContent);
+    const decryptedExtraData = decryptEncryptedData(
+      receipt.extra_data_content,
+      receipt.wrapped_receipt_key,
+      pem
+    );
+
+    return ExtraDataConverter.convertExtraData(decryptedExtraData.toString());
   }
 
   return new ExtraData(undefined);
