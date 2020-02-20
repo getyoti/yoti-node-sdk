49246b1496859c35076caa80b516a36dc99629e8
David Grayston <davidgrayston@googlemail.com>
2020-02-20 18:48:27 +0000

    SDK-1301: Create LivenessResourceResponse for unknown types

--- src/doc_scan_service/session/retrieve/liveness.resource.response.js
+++ src/doc_scan_service/session/retrieve/liveness.resource.response.js
@@ -3,13 +3,6 @@
 const ResourceResponse = require('./resource.response');
 
 class LivenessResourceResponse extends ResourceResponse {
-  constructor(resource) {
-    if (new.target === LivenessResourceResponse) {
-      throw TypeError(`${new.target.name} cannot be instantiated`);
-    }
-
-    super(resource);
-  }
 }
 
 module.exports = LivenessResourceResponse;
