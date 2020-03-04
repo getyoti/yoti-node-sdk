a5865960d1e093649cd6cbfa9eaf9fc3a67fa5d9
David Grayston <davidgrayston@googlemail.com>
2020-03-04 16:30:33 +0000

    SDK-1301: Add success page to demo

--- examples/doc-scan/config.js
+++ examples/doc-scan/config.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 module.exports = {
   YOTI_CLIENT_SDK_ID: process.env.YOTI_CLIENT_SDK_ID,
-  YOTI_KEY_FILE_PATH: fs.readFileSync(process.env.YOTI_KEY_FILE_PATH),
+  YOTI_PEM: fs.readFileSync(process.env.YOTI_KEY_FILE_PATH),
   YOTI_DOC_SCAN_IFRAME_URL: process.env.YOTI_DOC_SCAN_IFRAME_URL,
   YOTI_DOC_SCAN_API: process.env.YOTI_DOC_SCAN_API,
+  YOTI_APP_BASE_URL: process.env.YOTI_APP_BASE_URL,
 };
---
+++ examples/doc-scan/controllers/media.controller.js
@@ -0,0 +1,19 @@
+const config = require('../config');
+
+const {
+  DocScanClient,
+} = require('yoti');
+
+module.exports = async (req, res) => {
+  const docScanClient = new DocScanClient(
+    config.YOTI_CLIENT_SDK_ID,
+    config.YOTI_PEM,
+  );
+
+  try {
+    const media = await docScanClient.getMediaContent(req.session.DOC_SCAN_SESSION_ID, req.query.mediaId);
+    res.status(200).end(media.getContent().buffer);
+  } catch(error) {
+    res.render('pages/error', { error });
+  }
+};
