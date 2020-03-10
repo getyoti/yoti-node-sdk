0f2b273b14097fd944fcbf45fb79644ae2768777
David Grayston <davidgrayston@googlemail.com>
2020-03-10 20:47:06 +0000

    SDK-1301: Display generated media in example

--- examples/doc-scan/src/controllers/index.controller.js
+++ examples/doc-scan/src/controllers/index.controller.js
@@ -39,7 +39,7 @@ async function createSession() {
     )
     .withRequestedTask(
       new RequestedTextExtractionTaskBuilder()
-        .withManualCheckFallback()
+        .withManualCheckAlways()
         .build()
     )
     .withSdkConfig(
