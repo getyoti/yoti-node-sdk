8c22ebc49da84d2352e8354b63804dc17b37ac5f
David Grayston <davidgrayston@googlemail.com>
2020-03-10 21:31:43 +0000

    SDK-1301: Add example .env file

--- examples/doc-scan/config.js
+++ examples/doc-scan/config.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 module.exports = {
   YOTI_CLIENT_SDK_ID: process.env.YOTI_CLIENT_SDK_ID,
   YOTI_PEM: fs.readFileSync(process.env.YOTI_KEY_FILE_PATH),
-  YOTI_DOC_SCAN_IFRAME_URL: process.env.YOTI_DOC_SCAN_IFRAME_URL,
-  YOTI_DOC_SCAN_API: process.env.YOTI_DOC_SCAN_API,
-  YOTI_APP_BASE_URL: process.env.YOTI_APP_BASE_URL,
+  YOTI_DOC_SCAN_IFRAME_URL: process.env.YOTI_DOC_SCAN_IFRAME_URL || 'https://api.yoti.com/idverify/v1/web/index.html',
+  YOTI_APP_BASE_URL: process.env.YOTI_APP_BASE_URL || 'http://localhost:3000',
 };
