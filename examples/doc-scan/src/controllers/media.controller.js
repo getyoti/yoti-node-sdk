8bfafe04b91ef1c58b72910a9e0d40e620317e62
David Grayston <davidgrayston@googlemail.com>
2020-03-11 17:15:31 +0000

    SDK-1301: Only allow base64 decoding of application/octet-stream

--- examples/doc-scan/src/controllers/media.controller.js
+++ examples/doc-scan/src/controllers/media.controller.js
@@ -21,7 +21,7 @@ module.exports = async (req, res) => {
     let buffer = media.getContent().toBuffer();
 
     // If the media is base64 encoded, decode and detect the mime type.
-    if (req.query.base64 === '1') {
+    if (req.query.base64 === '1' && contentType === 'application/octet-stream') {
       buffer = Buffer.from(buffer.toString('utf8'), 'base64');
       const fileInfo = await FileType.fromBuffer(buffer);
       contentType = fileInfo.mime || contentType;
