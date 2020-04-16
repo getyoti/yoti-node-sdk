145f519731853194ddd834ee9ef1029bf7e26593
David Grayston <davidgrayston@googlemail.com>
2020-04-16 19:05:46 +0100

    SDK-1460: Fix default demo app URL

--- examples/doc-scan/config.js
+++ examples/doc-scan/config.js
@@ -4,5 +4,5 @@ module.exports = {
   YOTI_CLIENT_SDK_ID: process.env.YOTI_CLIENT_SDK_ID,
   YOTI_PEM: fs.readFileSync(process.env.YOTI_KEY_FILE_PATH),
   YOTI_DOC_SCAN_IFRAME_URL: process.env.YOTI_DOC_SCAN_IFRAME_URL || 'https://api.yoti.com/idverify/v1/web/index.html',
-  YOTI_APP_BASE_URL: process.env.YOTI_APP_BASE_URL || 'http://localhost:3000',
+  YOTI_APP_BASE_URL: process.env.YOTI_APP_BASE_URL || 'https://localhost:3000',
 };
