e1c24fc8995174935eb531e95873b83c8fad191c
David Grayston <davidgrayston@googlemail.com>
2020-05-04 20:55:05 +0100

    SDK-1301: Correct demo error message

--- examples/doc-scan/src/controllers/error.controller.js
+++ examples/doc-scan/src/controllers/error.controller.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  let error = 'An unknown error occured';
+  let error = 'An unknown error occurred';
 
   if (req.query.yotiErrorCode) {
     error = `Error Code: ${req.query.yotiErrorCode}`;
