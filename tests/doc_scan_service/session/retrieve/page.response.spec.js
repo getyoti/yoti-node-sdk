2d7bda26fce5db54d7e7e6760d58b34bcf0997e0
David Grayston <davidgrayston@googlemail.com>
2020-09-28 12:01:55 +0100

    SDK-1769: Add support for document page frames

--- tests/doc_scan_service/session/retrieve/page.response.spec.js
+++ tests/doc_scan_service/session/retrieve/page.response.spec.js
@@ -1,3 +1,4 @@
+const FrameResponse = require('../../../../src/doc_scan_service/session/retrieve/frame.response');
 const PageResponse = require('../../../../src/doc_scan_service/session/retrieve/page.response');
 const MediaResponse = require('../../../../src/doc_scan_service/session/retrieve/media.response');
 
@@ -8,6 +9,7 @@ describe('PageResponse', () => {
     pageResponse = new PageResponse({
       capture_method: 'some-capture-method',
       media: {},
+      frames: [{}, {}],
     });
   });
 
@@ -20,6 +22,26 @@ describe('PageResponse', () => {
   describe('#getMedia', () => {
     it('should return media', () => {
       expect(pageResponse.getMedia()).toBeInstanceOf(MediaResponse);
+    });
+  });
+
+  describe('#getFrames', () => {
+    describe('when frames are available', () => {
+      it('should return array of frames', () => {
+        const frames = pageResponse.getFrames();
+        expect(frames.length).toBe(2);
+        frames.forEach((frame) => {
+          expect(frame).toBeInstanceOf(FrameResponse);
+        });
+      });
+    });
+    describe('when frames are not available', () => {
+      it('should return empty array', () => {
+        pageResponse = new PageResponse({});
+        const frames = pageResponse.getFrames();
+        expect(frames).toBeInstanceOf(Array);
+        expect(frames.length).toBe(0);
+      });
     });
   });
 });
