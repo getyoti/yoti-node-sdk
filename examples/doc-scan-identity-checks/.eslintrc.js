a1bb01facb644ef9ca1feee08fb7abb4f9312064
Abdalmajeed Aljaradi <102966514+abdalmajeed-yoti@users.noreply.github.com>
2022-09-02 13:14:51 +0300

    SDK-2174 Upgrade examples dependencies (#333)
    * Upgraded examples dependencies
    
    * linted examples code with the updated eslint and eslint plugins
    
    * Updated lowest supported node version

--- examples/doc-scan-identity-checks/.eslintrc.js
+++ examples/doc-scan-identity-checks/.eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  "plugins": ["node"],
-  "extends": ["plugin:node/recommended", "airbnb-base"],
+  plugins: ['node'],
+  extends: ['plugin:node/recommended', 'airbnb-base'],
   rules: {
-    "no-console": 0,
-    "comma-dangle": ["error", {
-      "arrays": "always-multiline",
-      "exports": "always-multiline",
-      "functions": "never",
-      "imports": "always-multiline",
-      "objects": "always-multiline"
+    'no-console': 0,
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+      imports: 'always-multiline',
+      objects: 'always-multiline',
     }],
-    "function-paren-newline": ["error", "multiline-arguments"],
+    'function-paren-newline': ['error', 'multiline-arguments'],
   },
   env: {
-    "node": true
-  }
+    node: true,
+  },
+  root: true,
 };
