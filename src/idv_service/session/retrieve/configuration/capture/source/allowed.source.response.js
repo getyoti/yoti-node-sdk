d0357678e89abb4e5836cc9d631a02260b7d495b
Abdalmajeed Aljaradi <abdalmajeed.aljaradi@yoti.com>
2023-01-07 14:29:36 +0300

    Converted AllowedSourceResponse to simulate an Abstract class behaviour

--- src/idv_service/session/retrieve/configuration/capture/source/allowed.source.response.js
+++ src/idv_service/session/retrieve/configuration/capture/source/allowed.source.response.js
@@ -2,6 +2,11 @@ const Validation = require('../../../../../../yoti_common/validation');
 
 class AllowedSourceResponse {
   constructor(type) {
+    const currentClass = new.target;
+    if (currentClass === AllowedSourceResponse) {
+      throw new Error('AllowedSourceResponse can not be instantiated');
+    }
+
     Validation.isString(type, 'type');
     this.type = type;
   }
--- tests/idv_service/session/retrieve/configuration/capture/source/allowed.source.response.spec.js
+++ tests/idv_service/session/retrieve/configuration/capture/source/allowed.source.response.spec.js
@@ -1,10 +1,20 @@
 const IDVConstants = require('../../../../../../../src/idv_service/idv.constants');
+const AllowedSourceResponse = require('../../../../../../../src/idv_service/session/retrieve/configuration/capture/source/allowed.source.response');
 const EndUserAllowedSourceResponse = require('../../../../../../../src/idv_service/session/retrieve/configuration/capture/source/end.user.allowed.source.response');
 const IbvAllowedSourceResponse = require('../../../../../../../src/idv_service/session/retrieve/configuration/capture/source/ibv.allowed.source.response');
 const RelyingBusinessAllowedSourceResponse = require('../../../../../../../src/idv_service/session/retrieve/configuration/capture/source/relying.business.allowed.source.response');
 const UnknownAllowedSourceResponse = require('../../../../../../../src/idv_service/session/retrieve/configuration/capture/source/unknown.allowed.source.response');
 
 describe('AllowedSourceResponse', () => {
+  describe('is an abstract class', () => {
+    it('should not be instantiable', () => {
+      expect(() => {
+        // eslint-disable-next-line no-new
+        new AllowedSourceResponse();
+      }).toThrow('AllowedSourceResponse can not be instantiated');
+    });
+  });
+
   it('should get the right type', () => {
     const endUserSource = new EndUserAllowedSourceResponse();
     const relyingBusinessSource = new RelyingBusinessAllowedSourceResponse();
