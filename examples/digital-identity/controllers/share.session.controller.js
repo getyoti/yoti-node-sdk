baf9de6c38f1e56708c0546a7c533736099cec99
Dhivya Pattabhiraman <dhivya.pattabhiraman@yoti.com>
2023-02-14 14:35:23 +0000

    Added an example to create a share session using the digital identity service.

---
+++ examples/digital-identity/controllers/share.session.controller.js
@@ -0,0 +1,54 @@
+const {
+  DigitalIdentityBuilders:
+    {
+      PolicyBuilder,
+      LocationConstraintExtensionBuilder,
+      ShareSessionConfigurationBuilder,
+    },
+  DigitalIdentityClient,
+} = require('yoti');
+
+const config = require('../config');
+
+const digitalIdentityClient = new DigitalIdentityClient(config.CLIENT_SDK_ID, config.PEM_KEY);
+
+module.exports = (req, res) => {
+  const locationExtension = new LocationConstraintExtensionBuilder()
+    .withLatitude(51.5074)
+    .withLongitude(-0.1278)
+    .withRadius(6000)
+    .build();
+
+  const policy = new PolicyBuilder()
+    .withFullName()
+    .withEmail()
+    .withPhoneNumber()
+    .withSelfie()
+    .withStructuredPostalAddress()
+    .withAgeOver(18)
+    .withNationality()
+    .withGender()
+    .withDocumentDetails()
+    .withDocumentImages()
+    .withWantedRememberMe()
+    .build();
+
+  const subject = {
+    subject_id: 'some_subject_id_string',
+  };
+
+  const shareSessionConfig = new ShareSessionConfigurationBuilder()
+    .withRedirectUri('/profile')
+    .withPolicy(policy)
+    .withExtension(locationExtension)
+    .withSubject(subject)
+    .build();
+
+  digitalIdentityClient.createShareSession(shareSessionConfig)
+    .then((shareSessionCreateResult) => {
+      res.json({ ShareSessionResult: shareSessionCreateResult });
+    }).catch((error) => {
+      console.error({ status: error.status, message: error.response.text });
+      res.end();
+    });
+};
