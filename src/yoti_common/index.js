ce7beda44aa56b4d077346b111e4a1cb25cd83b4
Liam Debell <liam.debell@yoti.com>
2023-02-28 14:23:43 +0000

    Rename decomposition method for AES_GCM

--- src/yoti_common/index.js
+++ src/yoti_common/index.js
@@ -243,7 +243,7 @@ module.exports.decryptAsymmetric = (cipherText, pem) => {
  *
  * @returns {{ cipherText: Buffer, tag: Buffer }}
  */
-module.exports.decomposeAESGCMKey = (secret, tagSize = 16) => {
+module.exports.decomposeAESGCMSecret = (secret, tagSize = 16) => {
   const cipherText = secret.subarray(0, secret.length - tagSize);
   const tag = secret.subarray(secret.length - tagSize);
 
