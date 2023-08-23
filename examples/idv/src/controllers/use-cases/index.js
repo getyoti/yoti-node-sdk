d6ce6dcd3eb9dc66c4961803ac063b75f0ffb0d3
Dhivya Pattabhiraman <dhivya.pattabhiraman@yoti.com>
2023-08-23 16:53:50 +0100

    Updated the IDV example to allow non-Latin documents.

--- examples/idv/src/controllers/use-cases/index.js
+++ examples/idv/src/controllers/use-cases/index.js
@@ -1,5 +1,6 @@
 const authenticityAndIdentityCheckController = require('./authenticity.and.identity.check.controller');
 const documentComparisonCheckController = require('./document.comparison.check.controller');
+const allowNonLatinDocumentCheckController = require('./allow.non.latin.document.check.controller');
 const allowExpiredDocumentCheckController = require('./allow.expired.document.check.controller');
 const faceComparisonCheckController = require('./face.comparison.check.controller');
 const faceMatchCheckController = require('./face.match.check.controller');
@@ -8,6 +9,7 @@ const watchlistCheckController = require('./watchlist.check.controller');
 module.exports = {
   authenticityAndIdentityCheckController,
   documentComparisonCheckController,
+  allowNonLatinDocumentCheckController,
   allowExpiredDocumentCheckController,
   faceComparisonCheckController,
   faceMatchCheckController,
