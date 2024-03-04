c87cd1fb16c74a5dc3d0a3a0f94f3c9f6a754a06
laurent <laurent.deyherassary@yoti.com>
2024-03-04 12:26:50 +0000

    Surface the 'requirements not met details' within the IdentityProfileFailureReasonResponse.

--- types/src/idv_service/session/retrieve/identity.profile.failure.reason.response.d.ts
+++ types/src/idv_service/session/retrieve/identity.profile.failure.reason.response.d.ts
@@ -1,7 +1,31 @@
 export = IdentityProfileFailureReasonResponse;
+/**
+ * @typedef {Object} RequirementsNotMetDetail
+ * @property {string} [failureType]
+ * @property {string} [documentType]
+ * @property {string} [documentCountryIsoCode]
+ * @property {string} [auditId]
+ * @property {string} [details]
+ */
 declare class IdentityProfileFailureReasonResponse {
     constructor(failureReason: any);
     /** @private */
     private reasonCode;
+    /** @private  */
+    private requirementsNotMetDetails;
     getReasonCode(): any;
+    /**
+     * @returns {RequirementsNotMetDetail[]}
+     */
+    getRequirementsNotMetDetails(): RequirementsNotMetDetail[];
 }
+declare namespace IdentityProfileFailureReasonResponse {
+    export { RequirementsNotMetDetail };
+}
+type RequirementsNotMetDetail = {
+    failureType?: string;
+    documentType?: string;
+    documentCountryIsoCode?: string;
+    auditId?: string;
+    details?: string;
+};
