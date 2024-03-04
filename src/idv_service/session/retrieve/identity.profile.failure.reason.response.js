772d787d3d3825e0a34c5bc0da451880905bd08b
laurent <laurent.deyherassary@yoti.com>
2024-03-04 12:26:50 +0000

    Surface the 'requirements not met details' within the IdentityProfileFailureReasonResponse.

--- src/idv_service/session/retrieve/identity.profile.failure.reason.response.js
+++ src/idv_service/session/retrieve/identity.profile.failure.reason.response.js
@@ -2,15 +2,59 @@
 
 const Validation = require('../../../yoti_common/validation');
 
+/**
+ * @typedef {Object} RequirementsNotMetDetail
+ * @property {string} [failureType]
+ * @property {string} [documentType]
+ * @property {string} [documentCountryIsoCode]
+ * @property {string} [auditId]
+ * @property {string} [details]
+ */
+
 class IdentityProfileFailureReasonResponse {
   constructor(failureReason) {
     Validation.isString(failureReason.reason_code, 'reason code');
     /** @private */
     this.reasonCode = failureReason.reason_code;
+
+    /** @private  */
+    this.requirementsNotMetDetails = [];
+
+    // eslint-disable-next-line camelcase
+    const { requirements_not_met_details: requirementsNotMetDetails } = failureReason;
+    if (requirementsNotMetDetails) {
+      Validation.isArray(requirementsNotMetDetails, 'requirements not met details');
+
+      this.requirementsNotMetDetails = requirementsNotMetDetails.map((detail) => {
+        const {
+          failure_type: failureType,
+          document_type: documentType,
+          document_country_iso_code: documentCountryIsoCode,
+          audit_id: auditId,
+          details,
+        } = detail;
+
+        return ({
+          failureType,
+          documentType,
+          documentCountryIsoCode,
+          auditId,
+          details,
+        });
+      });
+    }
+    /** @private */
   }
 
   getReasonCode() {
     return this.reasonCode;
+  }
+
+  /**
+   * @returns {RequirementsNotMetDetail[]}
+   */
+  getRequirementsNotMetDetails() {
+    return this.requirementsNotMetDetails;
   }
 }
 
