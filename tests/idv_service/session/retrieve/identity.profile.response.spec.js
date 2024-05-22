b78de175b3c60f7234942b670ea18a6c6431aa1e
laurent-yoti <laurent.deyherassary@yoti.com>
2024-05-22 09:43:30 +0100

    SDK-2453 include requirements_not_met_details in scheme compliance (#472)
    * Created class IdentityProfileRequirementsNotMetDetailResponse to parse/expose the RequirementsNotMetDetail object.
    Updated IdentityProfileFailureReasonResponse to use IdentityProfileRequirementsNotMetDetailResponse
    Modified IdentityProfileReportSchemesComplianceResponse to include conditionally requirementsNotMetDetails, as a list of IdentityProfileRequirementsNotMetDetailResponse
    
    * Fixes for JSDoc/Typescript

--- tests/idv_service/session/retrieve/identity.profile.response.spec.js
+++ tests/idv_service/session/retrieve/identity.profile.response.spec.js
@@ -1,6 +1,7 @@
 const IdentityProfileResponse = require('../../../../src/idv_service/session/retrieve/identity.profile.response');
 const IdentityProfileReportResponse = require('../../../../src/idv_service/session/retrieve/identity.profile.report.response');
 const IdentityProfileFailureReasonResponse = require('../../../../src/idv_service/session/retrieve/identity.profile.failure.reason.response');
+const IdentityProfileRequirementsNotMetDetailResponse = require('../../../../src/idv_service/session/retrieve/identity.profile.requirements.not.met.detail.response');
 
 describe('IdentityProfileResponse', () => {
   let identityProfileResponse;
@@ -71,6 +72,7 @@ describe('IdentityProfileResponse', () => {
       expect(failureReason.getReasonCode()).toBe('MANDATORY_DOCUMENT_NOT_PROVIDED');
       expect(failureReason.getRequirementsNotMetDetails()).toHaveLength(2);
       const [firstDetail, secondDetail] = failureReason.getRequirementsNotMetDetails();
+      expect(firstDetail).toBeInstanceOf(IdentityProfileRequirementsNotMetDetailResponse);
       expect(firstDetail).toEqual(expect.objectContaining({
         failureType: 'ID_DOCUMENT_EXTRACTION',
         documentType: 'PASSPORT',
@@ -78,6 +80,7 @@ describe('IdentityProfileResponse', () => {
         auditId: 'audit-123',
         details: 'something not right',
       }));
+      expect(firstDetail).toBeInstanceOf(IdentityProfileRequirementsNotMetDetailResponse);
       expect(secondDetail).toEqual(expect.objectContaining({
         failureType: 'ID_DOCUMENT_AUTHENTICITY',
         documentType: 'PASSPORT',
