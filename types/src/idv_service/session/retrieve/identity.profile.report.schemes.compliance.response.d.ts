b06562b03342dcbe3d0c53c8e91a5ebfb6d83250
laurent <laurent.deyherassary@yoti.com>
2024-06-04 08:48:36 +0100

    SDK-2453 revert parsing of requirements_not_met_details in scheme compliance, keeps IdentityProfileRequirementsNotMetDetailResponse class

--- types/src/idv_service/session/retrieve/identity.profile.report.schemes.compliance.response.d.ts
+++ types/src/idv_service/session/retrieve/identity.profile.report.schemes.compliance.response.d.ts
@@ -7,14 +7,10 @@ declare class IdentityProfileReportSchemesComplianceResponse {
     private requirementsMet;
     /** @private @type {string|undefined} */
     private requirementsNotMetInfo;
-    /** @private @type {IdentityProfileRequirementsNotMetDetailResponse[]|undefined} */
-    private requirementsNotMetDetails;
     /**
      * @returns {object}
      */
     getScheme(): object;
     isRequirementsMet(): boolean;
     getRequirementsNotMetInfo(): string;
-    getRequirementsNotMetDetails(): IdentityProfileRequirementsNotMetDetailResponse[];
 }
-import IdentityProfileRequirementsNotMetDetailResponse = require("./identity.profile.requirements.not.met.detail.response");
