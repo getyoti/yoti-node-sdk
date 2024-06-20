2df20c3d6617d4ebb212364bcdd3f4f515f90e15
laurent <laurent.deyherassary@yoti.com>
2024-06-20 09:35:07 +0100

    Updated examples /examples/idv-identity-checks to use advanced identity profile (to be reviewed)

--- examples/idv-identity-checks/src/controllers/index.controller.js
+++ examples/idv-identity-checks/src/controllers/index.controller.js
@@ -2,6 +2,9 @@ const {
   IDVClient,
   SessionSpecificationBuilder,
   SdkConfigBuilder,
+  AdvancedIdentityProfileBuilder,
+  AdvancedIdentityProfileRequirementsBuilder,
+  AdvancedIdentityProfileSchemeBuilder,
 } = require('yoti');
 const config = require('../../config');
 
@@ -23,12 +26,47 @@ async function createSession() {
     },
   };
 
+  const advancedIdentityProfileSchemeDBS = new AdvancedIdentityProfileSchemeBuilder()
+    .withType('DBS')
+    .withObjective('BASIC')
+    .withLabel('lalala-DBS')
+    .build();
+
+  const advancedIdentityProfileSchemeRTW = new AdvancedIdentityProfileSchemeBuilder()
+    .withType('RTW')
+    .withLabel('lalala-RTW')
+    .build();
+
+  const advancedIdentityProfileUKTFIDA = new AdvancedIdentityProfileBuilder()
+    .withTrustFramework('UK_TFIDA')
+    .withScheme(advancedIdentityProfileSchemeDBS)
+    .withScheme(advancedIdentityProfileSchemeRTW)
+    .build();
+
+  const advancedIdentityProfileSchemeYGI = new AdvancedIdentityProfileSchemeBuilder()
+    .withType('IDENTITY')
+    .withObjective('AL_L1')
+    .withLabel('my-idendity-AL-L1')
+    .build();
+
+  const advancedIdentityProfileYotiGlobal = new AdvancedIdentityProfileBuilder()
+    .withTrustFramework('YOTI_GLOBAL')
+    .withScheme(advancedIdentityProfileSchemeYGI)
+    .withScheme(advancedIdentityProfileSchemeRTW)
+    .build();
+
+  const advancedIdentityProfileRequirements = new AdvancedIdentityProfileRequirementsBuilder()
+    .withProfile(advancedIdentityProfileUKTFIDA)
+    .withProfile(advancedIdentityProfileYotiGlobal)
+    .build();
+
   const sessionSpec = new SessionSpecificationBuilder()
     .withClientSessionTokenTtl(600) // 10 minutes
     .withResourcesTtl(90000) // session TTL(10 minutes) + 24 hours(minimum required)
     .withUserTrackingId('some-user-tracking-id')
     .withSubject(subject)
-    .withIdentityProfileRequirements(identityProfileRequirements)
+    // .withIdentityProfileRequirements(identityProfileRequirements)
+    .withAdvancedIdentityProfileRequirements(advancedIdentityProfileRequirements)
     .withSdkConfig(
       new SdkConfigBuilder()
         .withPrimaryColour('#2d9fff')
@@ -37,6 +75,7 @@ async function createSession() {
         .withSuccessUrl(`${config.YOTI_APP_BASE_URL}/success`)
         .withErrorUrl(`${config.YOTI_APP_BASE_URL}/error`)
         .withAllowHandoff(true)
+        .withAllowsCameraAndUpload()
         .build()
     )
     .build();
