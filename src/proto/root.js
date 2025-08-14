01e3de9b40c4da364665ae31dac07900f1cde57e
tmerse <mail@tmerse.com>
2025-08-14 14:48:58 +0200

    fix: remove readdirsync and add hardcoded proto files

--- src/proto/root.js
+++ src/proto/root.js
@@ -1,24 +1,22 @@
-const fs = require('fs');
 const path = require('path');
 const ProtoBuf = require('protobufjs');
 
-function getFilesPathsRecursively(directory, origin) {
-  return fs.readdirSync(directory)
-    .reduce((files, file) => {
-      const absolute = path.join(directory, file);
-      return [
-        ...files,
-        ...(fs.statSync(absolute).isDirectory()
-          ? getFilesPathsRecursively(absolute, origin || directory)
-          : [path.relative(origin || directory, absolute)]),
-      ];
-    }, [])
-    .filter((file) => file.endsWith('.proto'));
-}
+// Hardcoded list of proto files to avoid readDirSync for edge function compatibility
+const protoFiles = [
+  'attribute-public-api/attrpubapi_v1/Attribute.proto',
+  'attribute-public-api/attrpubapi_v1/ContentType.proto',
+  'attribute-public-api/attrpubapi_v1/List.proto',
+  'attribute-public-api/attrpubapi_v1/Signing.proto',
+  'common-public-api/compubapi_v1/EncryptedData.proto',
+  'common-public-api/compubapi_v1/SignedTimestamp.proto',
+  'share-public-api/sharepubapi_v1/DataEntry.proto',
+  'share-public-api/sharepubapi_v1/ExtraData.proto',
+  'share-public-api/sharepubapi_v1/IssuingAttributes.proto',
+  'share-public-api/sharepubapi_v1/ThirdPartyAttribute.proto',
+];
 
 const definitionsPath = path.join(__dirname, 'definitions');
-const definitionsFiles = getFilesPathsRecursively(definitionsPath)
-  .map((file) => path.join(definitionsPath, file));
+const definitionsFiles = protoFiles.map((file) => path.join(definitionsPath, file));
 
 const root = ProtoBuf.loadSync(definitionsFiles);
 
